47a25b97eeabdc5d2b2d80ff689c7be15757daa2
kai63001 <animehq5@gmail.com>
2021-02-08 01:43:40 +0700

    add alert image

--- lib/components/alert.dart
+++ lib/components/alert.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
-Future<void> buildShowDialog(BuildContext context,String text) {
-    return showDialog<void>(
-        context: context,
-        barrierDismissible: false, // user must tap button!
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  Text(text),
-                ],
+Future<void> buildShowDialog(BuildContext context, String text) {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(text,style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
+              Center(
+                child: Image.asset(
+                  'assets/images/alert.png',
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                ),
               ),
-            ),
-          );
-        },
+              Container(
+                child: Text("asd"),
+              )
+            ],
+          ),
+        ),
       );
-  }
+    },
+  );
+}
