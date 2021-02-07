9378a4db86d95c86f2d742b6e88b2efc4302ec8e
kai63001 <animehq5@gmail.com>
2021-02-07 15:56:22 +0700

    update

--- backend/src/controller/main/main.controller.ts
+++ backend/src/controller/main/main.controller.ts
@@ -3,17 +3,17 @@ import express, {Application, Request, Response, NextFunction} from 'express';
 class UsersController {
 
   public router: express.Router = express.Router();
-  public path: string = '/as';
+  public path: string = '/';
 
   constructor() {
     this.initializeRoutes();
   }
 
   public initializeRoutes() {
-    this.router.get('/', this.getAllUsers);
+    this.router.get('/as', this.getAllUsers);
   }
 
-  async getAllUsers(
+  private async getAllUsers(
     req: Request,
     res: Response,
     next: NextFunction
