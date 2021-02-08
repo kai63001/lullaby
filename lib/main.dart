ec32ee011ef0ffa8e177e3b0e8feabb7e9703720
kai63001 <animehq5@gmail.com>
2021-02-09 02:11:40 +0700

    chage title

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lullaby',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'FredokaOne',
