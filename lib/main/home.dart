e5e1f297ed71be3ebcf82efd02bc17dbf5c06daa
kai63001 <animehq5@gmail.com>
2021-02-10 00:13:45 +0700

    update main navbar list

--- lib/main/home.dart
+++ lib/main/home.dart
@@ -1,6 +1,8 @@
+import 'package:Lullaby/main/widget/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,13 +10,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Lullaby',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        splashColor: Colors.transparent,
         fontFamily: 'FredokaOne',
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -33,6 +35,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white);
+  List<Widget> _widgetOptions = <Widget>[
+    WidgetMain(),
+    Text(
+      'Index 1: Businessss',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  String token = 'asd';
+
+  @override
+  void initState() {
+    super.initState();
+    this._incrementCounter();
+  }
+
+  _incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      token = prefs.getString('token');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,18 +87,71 @@ class _MyHomePageState extends State<MyHomePage> {
         systemNavigationBarColor: Color(0xff1e1e2a),
         systemNavigationBarIconBrightness: Brightness.dark));
     return Scaffold(
-      backgroundColor: Color(0xff1e1e2a),
+      backgroundColor:
+          _selectedIndex != 0 ? Color(0xff1e1e2a) : Color(0xff252736),
       appBar: AppBar(
-        brightness: Brightness.dark, // status bar brightness
+          brightness: Brightness.dark, // status bar brightness
+          backgroundColor: Color(0xff1e1e2a),
+          centerTitle: true,
+          title: Text(
+            widget.title,
+            style: TextStyle(color: Colors.white),
+          ),
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(CupertinoIcons.equal),
+            onPressed: () {},
+          ),
+          actions: [
+            // action button
+            IconButton(
+              icon: Icon(CupertinoIcons.bag),
+              onPressed: () {},
+            ),
+          ]),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 0
+                ? Icon(CupertinoIcons.house_alt_fill)
+                : Icon(CupertinoIcons.house_alt),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 1
+                ? Icon(CupertinoIcons.search)
+                : Icon(CupertinoIcons.search),
+            label: 'search',
+          ),
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 2
+                ? Icon(CupertinoIcons.plus)
+                : Icon(CupertinoIcons.plus),
+            label: 'add',
+          ),
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 3
+                ? Icon(CupertinoIcons.bell_fill)
+                : Icon(CupertinoIcons.bell),
+            label: 'notification',
+          ),
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 4
+                ? Icon(CupertinoIcons.person_fill)
+                : Icon(CupertinoIcons.person),
+            label: 'profile',
+          ),
+        ],
+        unselectedItemColor: Color(0xff4f4e5d),
+        selectedItemColor: Colors.white,
         backgroundColor: Color(0xff1e1e2a),
-        centerTitle: true,
-        // title: Text(widget.title,style: TextStyle(color: Colors.white),),
-        elevation: 0,
+        onTap: _onItemTapped,
       ),
-      body: Container(
-        child: Text("fuck")
-      ),
-      
     );
   }
 }
---
+++ lib/main/widget/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WidgetMain extends StatelessWidget {
+  const WidgetMain({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("asdsasd");
+  }
+}
