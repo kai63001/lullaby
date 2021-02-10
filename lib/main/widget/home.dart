b3d2eb9649bbe8111e954a487279d8308f57dcc9
kai63001 <animehq5@gmail.com>
2021-02-10 18:53:34 +0700

    update avatar and card

--- lib/main/widget/home.dart
+++ lib/main/widget/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WidgetMain extends StatelessWidget {
@@ -7,6 +8,108 @@ class WidgetMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("asdsasda");
+    return ListView(
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Container(
+              padding: EdgeInsets.all(20),
+              color: Color(0xff252736),
+              child: Text(
+                "asdsasda",
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
+        for (var item in [1, 2, 3, 4, 5,7,8,9])
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Container(
+                padding:
+                    EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 20),
+                color: Color(0xff252736),
+                child: Container(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: Color(0xff7549fd),
+                                ),
+                                width: 50,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image.asset(
+                                      'assets/images/avatars/monster.png'),
+                                )),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "SadBoy SomeTime",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 18),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "10 hours ago",
+                                        style: TextStyle(
+                                            color: Colors.white54,
+                                            fontSize: 12),
+                                      ),
+                                      Text(
+                                        " · ",
+                                        style: TextStyle(
+                                            color: Colors.white54,
+                                            fontSize: 12),
+                                      ),
+                                      Text(
+                                        "Sad",
+                                        style: TextStyle(
+                                            color: Colors.redAccent,
+                                            fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // Padding(
+                            //   padding: const EdgeInsets.only(left: 10.0),
+                            //   child: Text(
+                            //         "is Sad",
+                            //         style: TextStyle(
+                            //             color: Colors.white, fontSize: 18),
+                            //       ),
+                            // ),
+                          ],
+                        ),
+                        Icon(
+                          CupertinoIcons.ellipsis_vertical,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Text(
+                        "Fuck my life",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ))),
+          ),
+      ],
+    );
   }
 }
