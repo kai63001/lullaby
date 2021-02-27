c869ed0514a812b6114293b50210ffa359bd9304
aldestra <66737415+aldestra@users.noreply.github.com>
2021-02-27 13:46:55 +0700

    update microservice

--- backend/profile/src/controller/profile/profile.controller.ts
+++ backend/profile/src/controller/profile/profile.controller.ts
@@ -14,6 +14,7 @@ class ProfileController {
 
   public initializeRoutes() {
     this.router.get("/profile", authRequest, this.getMyProfile);
+    this.router.get("/profile2", authRequest, this.getMyProfile);
   }
 
   private async getMyProfile(req: Request, res: Response, next: NextFunction) {
