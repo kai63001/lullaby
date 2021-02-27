179c4fc08523cfadb45daa58c63d7f1fb529b00b
aldestra <66737415+aldestra@users.noreply.github.com>
2021-02-27 10:06:25 +0700

    update git

--- backend/post/dist/middleware/test.md.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function loggerMiddleware(request, response, next) {
-    response.send(request.body);
-}
-exports.default = loggerMiddleware;
